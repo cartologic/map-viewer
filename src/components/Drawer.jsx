9bd1c438da9eddf22106c9d365272c2599b9a12a
hisham waleed karam <hisham.karam@cartologic.com>
2019-04-17 19:27:45 +0200

    arab demo

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react'
 
 import AddIcon from '@material-ui/icons/Add'
 import AddLayers from './AddLayers'
+import BarChartIcon from '@material-ui/icons/BarChart'
 import CartoviewLayerSwitcher from './LayerSwitcher'
 import CartoviewLegends from './Legend'
 import CollapsibleListItem from './CollapsibleItem'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import ImageIcon from '@material-ui/icons/Image'
 import LayersIcons from '@material-ui/icons/Layers'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import MapIcon from '@material-ui/icons/Map'
+import MapRoundedIcon from '@material-ui/icons/MapRounded'
 import NavBar from './Navbar.jsx'
 import Paper from '@material-ui/core/Paper'
+import PrintIcon from '@material-ui/icons/Print'
 import PropTypes from 'prop-types'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import SaveDialog from './SaveDialog'
 import SaveIcon from '@material-ui/icons/Save'
+import TextField from '@material-ui/core/TextField';
 import classnames from 'classnames'
 import { withStyles } from '@material-ui/core/styles'
 
@@ -30,6 +38,10 @@ const styles = theme => ({
 	},
 	button: {
 		margin: theme.spacing.unit,
+	},
+	baseMaps: {
+		display: 'flex',
+		margin: '20px'
 	}
 })
 
@@ -56,12 +68,55 @@ const CartoviewDrawer = (props) => {
 						</ListItemIcon>
 						<ListItemText primary="Add Layers" />
 					</ListItem>
+					<CollapsibleListItem open={false} title="Maps" icon={<MapIcon />}>
+						<div>
+							<TextField
+								id="outlined-name"
+								label="Maps"
+								fullWidth
+								className={classes.textField}
+								margin="normal"
+								variant="outlined"
+							/>
+						</div>
+					</CollapsibleListItem>
+					<CollapsibleListItem open={false} title="Indicators" icon={<BarChartIcon />}>
+						<div>
+							<TextField
+								id="outlined-name"
+								label="Indicators"
+								fullWidth
+								className={classes.textField}
+								margin="normal"
+								variant="outlined"
+							/>
+						</div>
+					</CollapsibleListItem>
 					<CollapsibleListItem open={false} title="Layers" icon={<LayersIcons />}>
 						<CartoviewLayerSwitcher />
 					</CollapsibleListItem>
 					<CollapsibleListItem open={false} title="Legend" icon={<ImageIcon />}>
 						<CartoviewLegends />
 					</CollapsibleListItem>
+					<CollapsibleListItem open={false} title="Base Maps" icon={<MapRoundedIcon />}>
+						<div className={classes.baseMaps}>
+							<RadioGroup
+								aria-label="Gender"
+								name="gender1"
+								className={classes.group}
+							>
+								<FormControlLabel value="female" control={<Radio />} label="Base Map 1" />
+								<FormControlLabel value="male" control={<Radio />} label="Base Map 2" />
+								<FormControlLabel value="other" control={<Radio />} label="Base Map 3" />
+							</RadioGroup>
+						</div>
+					</CollapsibleListItem>
+					<ListItem button onClick={() => { }}>
+						<ListItemIcon>
+							<PrintIcon />
+						</ListItemIcon>
+						<ListItemText primary="Print" />
+					</ListItem>
 				</List>}
 				{component == "AddLayers" && <AddLayers setComponent={setComponent} />}
 			</Paper>
